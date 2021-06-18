9f468a0b04b643f178cb94b2e36b04a333906567
Romain Lefranc <lefranc46@gmail.com>
2021-06-18 14:03:00 +0400

    ajout @method patch formulaire update

--- laravel-base/resources/views/role/edit.blade.php
+++ laravel-base/resources/views/role/edit.blade.php
@@ -6,6 +6,7 @@
         <h1 class="text-center">Modifier un rôle</h1>
         <form action="{{ route('role.update',$role->id) }}" method="POST">
             @csrf
+            @method("patch")
             <div class="form-floating mb-3 col">
                 <input type="text" class="form-control" name="name" placeholder="Doe" value="{{$role->name}}" required pattern="[A-Za-z-]+">
                 <label for="floatingInput">Nom *</label>
