2a6fe97b3df0b31eb5e99f9a65ebff840a1b64ae
AndryRaza <70998539+AndryRaza@users.noreply.github.com>
2021-06-18 11:15:32 +0400

    Ajout liste des utilisateurs

--- laravel-base/resources/views/users/index.blade.php
+++ laravel-base/resources/views/users/index.blade.php
@@ -1,5 +1,27 @@
-<a href="/users/create">Aller au formulaire de création d'utilisateurs</a>
+@extends('layout')
+@section('content')
+   
+     <a href="/users/create"><button class="btn btn-primary mt-5">Créer un utilisateur</button></a>
 
-@foreach ($users as $user)
-   <a href="{{ route('users.edit',$user->id)}}"> {{$user->lastName}} </a>
-@endforeach
+    <h1>Liste des utilisateurs</h1>
+
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Nom</th>
+                <th scope="col">Prénom</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                @foreach ($users as $user)
+                    <th scope="row"> <a href="{{ route('users.edit', $user->id) }}"> {{ $user->id }} </a></th>
+                    <td>{{ $user->lastName }}</td>
+                    <td>{{ $user->firstName }}</td>
+                @endforeach
+            </tr>
+        </tbody>
+    </table>
+
+@endsection
