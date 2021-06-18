0d7a09920fdb22a76f0a7213c3ef6bfba167b84b
Romain Lefranc <lefranc46@gmail.com>
2021-06-18 11:47:28 +0400

    Ajout route role

--- laravel-base/routes/web.php
+++ laravel-base/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ActivityController;
+use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -21,3 +22,4 @@ Route::get('/', function () {
 
 Route::resource('activity', ActivityController::class);
 Route::resource('users',UserController::class);
+Route::resource('role', RoleController::class);
