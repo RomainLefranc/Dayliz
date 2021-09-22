36d203f685e368678600bec35a63f02952168fab
RomainLefranc <lefranc46@gmail.com>
2021-09-22 11:34:18 +0400

    modif template

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,7 +1,4 @@
-@extends('layout')
-@section('content')
-    
-<x-app-layout>
+   <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight d-flex justify-content-between align-items-center ">
             Utilisateurs
